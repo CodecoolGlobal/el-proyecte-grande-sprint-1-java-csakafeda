190234b53cddee395f95679081c78c368dfeb4b1
BrownieMaar <barnamarci07@gmail.com>
2023-01-31 13:52:48 +0100

    add 2 placeholder buttons to header

--- frontend/src/Pages/Layout.jsx
+++ frontend/src/Pages/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { getUserName, isUserSignedIn } from "../Tools/userTools";
 import "./Layout.css"
+import Button from "@mui/material/Button"
 
 
 export default function Layout() {
@@ -15,8 +16,12 @@ export default function Layout() {
                 </>
                 :
                 <>
-                    <div>Sign in</div>
-                    <div>Register</div>
+                    <Button href="#" variant="contained" sx={{ my: 1, mx: 1.5 }}>
+                        Login
+                    </Button>
+                    <Button href="#" color="secondary" variant="contained" sx={{ my: 1, mx: 1.5 }}>
+                        Sign up
+                    </Button>
                 </>
             }
         </header>
