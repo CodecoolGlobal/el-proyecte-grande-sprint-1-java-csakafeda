b7717f514f74b3242f070b2c6f37830f47362b59
BrownieMaar <barnamarci07@gmail.com>
2023-01-31 11:53:18 +0100

    add basic routes for app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
 
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HomePage from "./Pages/HomePage/HomePage.jsx"
+import Layout from './Pages/Layout'
 
 function App() {
 
   return (
     <div className="App">
-      Quiz.io
+    <Router>
+      <Routes>
+
+        <Route element={<Layout />}>
+
+          <Route path='/' element={<HomePage />} />
+        </Route>
+
+      </Routes>
+    </Router>
+
     </div>
   )
 }
