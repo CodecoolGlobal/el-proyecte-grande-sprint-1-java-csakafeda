a6acf20af3dcb73170ef04e39695a76700436c18
marto <marton.szak@gmail.com>
2023-01-31 09:31:20 +0100

    creates backend starter repo.

---
+++ backend/src/main/java/com/example/quizio/QuizioApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizioApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/quizio/QuizioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
