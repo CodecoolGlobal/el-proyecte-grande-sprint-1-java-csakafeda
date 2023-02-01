5bc7d39e7836e9d6367faaae62c07b4a54bf796e
BrownieMaar <barnamarci07@gmail.com>
2023-02-01 09:19:02 +0100

    add mui header

--- frontend/src/Pages/Layout.jsx
+++ frontend/src/Pages/Layout.jsx
@@ -1,30 +1,47 @@
-import { Outlet } from "react-router-dom";
-import { getUserName, isUserSignedIn } from "../Tools/userTools";
+import { Outlet, useNavigate } from "react-router-dom";
+import { flipUserSignInStatus, getUserName, isUserSignedIn } from "../Tools/userTools";
 import "./Layout.css"
 import Button from "@mui/material/Button"
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
 
 export default function Layout() {
+    const navigate = useNavigate();
 
     return <>
-        <header>
-            <div>Quiz.io</div>
-            {isUserSignedIn() ?
-                <>
-                    <div>{getUserName()}</div>
-                    <div>Statistics</div>
-                </>
-                :
-                <>
-                    <Button href="#" variant="contained" sx={{ my: 1, mx: 1.5 }}>
-                        Login
-                    </Button>
-                    <Button href="#" color="secondary" variant="contained" sx={{ my: 1, mx: 1.5 }}>
-                        Sign up
-                    </Button>
-                </>
-            }
-        </header>
+        <AppBar
+            position="static"
+            color="default"
+            elevation={0}
+            sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+        >
+            <Toolbar sx={{ flexWrap: 'wrap' }}>
+                <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+                    Quiz.io
+                </Typography>
+                {isUserSignedIn() ?
+                    <>
+                        <nav>
+                            <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+                                Profile
+                            </Button>
+                            <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }} onClick={() => navigate("/")}>
+                                Sign out
+                            </Button>
+                        </nav>
+                    </>
+                    :
+                    <>
+                        <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }} onClick={() => navigate("/")}>
+                            Login
+                        </Button>
+                        <Button href="#" color="secondary" variant="outlined" sx={{ my: 1, mx: 1.5 }} onClick={() => navigate("/")}>
+                            Sign up
+                        </Button>
+                    </>
+                }
+            </Toolbar>
+        </AppBar>
         <Outlet />
     </>
 }
