6d464fca87f3742762a524eb9f8a3f6fb4e9385a
BrownieMaar <barnamarci07@gmail.com>
2023-02-01 12:03:50 +0100

    add middle mouse easter egg to question page

--- frontend/src/Pages/Question/Counter.jsx
+++ frontend/src/Pages/Question/Counter.jsx
@@ -2,10 +2,11 @@ import { Typography } from "@mui/material";
 import Countdown from "react-countdown";
 
 export default function Counter({time, onTick, onComplete}) {
+    const middleMouseEasterEgg = mouseDownEvent => (mouseDownEvent && mouseDownEvent.button == 1) ? onComplete() : null;
 
     return <Typography align="center">
     <div className="counter">
-        <Countdown date={Date.now() + time} onTick={onTick} onComplete={onComplete} renderer={props => <div>{props.total / 1000}</div>} autoStart />
+        <Countdown date={Date.now() + time} onTick={onTick} onComplete={onComplete} renderer={props => <div onMouseDown={middleMouseEasterEgg}>{props.total / 1000}</div>} autoStart />
     </div>
 </Typography>
 }
