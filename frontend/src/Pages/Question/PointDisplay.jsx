3e3be28fcd1583c059a82b27b5669b99ba34d36f
BrownieMaar <barnamarci07@gmail.com>
2023-02-01 12:07:21 +0100

    make point component non-selectable

--- frontend/src/Pages/Question/PointDisplay.jsx
+++ frontend/src/Pages/Question/PointDisplay.jsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material";
 
 export default function PointDisplay({points}) {
 
-    return <Box>
+    return <Box sx={{userSelect: "none"}}>
     <Typography variant="h1" align="center">
         {points}
     </Typography>
