237a22c85864386d099724a0e242ed1f3ae2ec58
BrownieMaar <barnamarci07@gmail.com>
2023-02-02 06:59:40 +0100

    move the counter easter egg to entire div

--- frontend/src/Pages/Question/Counter.jsx
+++ frontend/src/Pages/Question/Counter.jsx
@@ -5,11 +5,11 @@ export default function Counter({ time, onTick, onComplete, isFinished }) {
     const middleMouseEasterEgg = mouseDownEvent => (mouseDownEvent && mouseDownEvent.button == 1) ? onComplete() : null;
 
     return <Typography align="center">
-        <div className="counter">
+        <div className="counter" onMouseDown={middleMouseEasterEgg}>
             {isFinished ?
                 <div className="stop"></div>
                 :
-                <Countdown date={Date.now() + time} onTick={onTick} onComplete={onComplete} renderer={props => <span onMouseDown={middleMouseEasterEgg}>{props.total / 1000}</span>} autoStart />
+                <Countdown date={Date.now() + time} onTick={onTick} onComplete={onComplete} renderer={props => <span >{props.total / 1000}</span>} autoStart />
             }
         </div>
     </Typography>
