15dccd84b2a1a686fadc28ae9442289a8c791f03
BrownieMaar <barnamarci07@gmail.com>
2023-02-02 06:37:26 +0100

    fix validateDomNesting realtions

--- frontend/src/Pages/Question/Counter.jsx
+++ frontend/src/Pages/Question/Counter.jsx
@@ -1,12 +1,16 @@
 import { Typography } from "@mui/material";
 import Countdown from "react-countdown";
 
-export default function Counter({time, onTick, onComplete}) {
+export default function Counter({ time, onTick, onComplete, isFinished }) {
     const middleMouseEasterEgg = mouseDownEvent => (mouseDownEvent && mouseDownEvent.button == 1) ? onComplete() : null;
 
     return <Typography align="center">
-    <div className="counter">
-        <Countdown date={Date.now() + time} onTick={onTick} onComplete={onComplete} renderer={props => <div onMouseDown={middleMouseEasterEgg}>{props.total / 1000}</div>} autoStart />
-    </div>
-</Typography>
+        <div className="counter">
+            {isFinished ?
+                <div className="stop"></div>
+                :
+                <Countdown date={Date.now() + time} onTick={onTick} onComplete={onComplete} renderer={props => <span onMouseDown={middleMouseEasterEgg}>{props.total / 1000}</span>} autoStart />
+            }
+        </div>
+    </Typography>
 }
