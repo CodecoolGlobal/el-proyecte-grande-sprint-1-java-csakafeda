b22ac82d101ec2283e3d518b7ba1120d7796bc6b
bridgend84 <sacrun@gmail.com>
2023-02-15 10:49:08 +0100

    Fix gitignore and delete unused imports

--- backend/src/main/java/com/example/quizio/database/AnswerDB.java
+++ backend/src/main/java/com/example/quizio/database/AnswerDB.java
@@ -1,25 +1,7 @@
 package com.example.quizio.database;
 
 import com.example.quizio.controller.dto.Answer;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashSet;
-import java.util.Set;
-
-@Repository
-public class AnswerDB {
-
-    Set<Answer> correctAnswers;
-
-    public AnswerDB() {
-        correctAnswers = new HashSet<>();
-    }
-
-    public Set<Answer> getCorrectAnswers() {
-        return correctAnswers;
-    }
-
-    public void addToAnswerDB(Answer answer) {
-        correctAnswers.add(answer);
-    }
+public interface AnswerDB extends JpaRepository<Answer, Long> {
 }
