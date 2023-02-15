2c0867af541715da78c5d279d4ea229fdd2e7b67
bridgend84 <sacrun@gmail.com>
2023-02-15 12:03:19 +0100

    feat question table

---
+++ backend/src/main/java/com/example/quizio/database/repository/Question.java
@@ -0,0 +1,24 @@
+package com.example.quizio.database.repository;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "question")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Question {
+    @Id
+    private String id;
+    private String question;
+    private String correctAnswer;
+    private String incorrectAnswer1;
+    private String incorrectAnswer2;
+    private String incorrectAnswer3;
+    private String category;
+    private String difficulty;
+}
