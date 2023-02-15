8e86540f25113907c56caa74ca263c3224b4e074
Feodóra Bakó <bako.feodora97@gmail.com>
2023-02-15 14:04:45 +0100

    add category and difficulty selection to homepage

--- frontend/src/Pages/Question/QuestionPageSingle.jsx
+++ frontend/src/Pages/Question/QuestionPageSingle.jsx
@@ -1,5 +1,5 @@
-import { Button, CircularProgress, Container, Grid } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import {Button, Container, Grid} from "@mui/material";
+import {useEffect, useRef, useState} from "react";
 import Loading from "../../Components/Loading";
 import Counter from "./Counter";
 import PointDisplay from "./PointDisplay";
@@ -8,20 +8,20 @@ import "./QuestionPage.css"
 const URL = "/api/question";
 
 
-export default function QuestionPage() {
+export default function QuestionPageSingle() {
     const [question, setQuestion] = useState(null);
     const dataFetchedRef = useRef(false);
 
     const [loading, setLoading] = useState(true);
     const [isTimeOut, setIsTimeOut] = useState(false);
     const [isFinished, setIsFinished] = useState(false);
-    
+
     const [points, setPoints] = useState(0);
     const [correctAnswerIndex, setCorrectAnswerIndex] = useState(null);
-    
+
     const TIME_FOR_QUESTION = 10000;
     let timeLeft = TIME_FOR_QUESTION;
-    
+
     async function fetchQuestion() {
         const response = await fetch(URL);
         const data = await response.json();
@@ -32,7 +32,7 @@ export default function QuestionPage() {
     useEffect(() => {
         if (dataFetchedRef.current) return;
         dataFetchedRef.current = true;
-        fetchQuestion()
+        fetchQuestion();
     }, [timeLeft, loading, correctAnswerIndex]);
 
     const submitAnswerAndGetCorrectIndex = async index => {
@@ -40,7 +40,7 @@ export default function QuestionPage() {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
-              },
+            },
             body: JSON.stringify({
                 questionId: question.id,
                 answerIndex: index
@@ -84,24 +84,30 @@ export default function QuestionPage() {
         <div>
 
             <Container maxWidth="md" sx={{minHeight: 250}}>
-                {isTimeOut ? <Container maxWidth="md" align="center"><h1>You weren't fast enough!</h1></Container> : loading ? <h1><Loading /></h1> : <>
-                    <h1 align="center">{question.question}</h1>
-                    <Grid
-                        container
-                        textAlign="center"
-                        spacing={3}
-                        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-                    >
-                        {question.answers.map((answer, index) => (
-                            <Grid key={index} item xs={6}>
-                                <Button variant="contained" className={correctAnswerIndex === index ? "correct" : null} sx={{ width: "100%" }} size="large" key={index} onClick={() => handleAnswerSubmit(index)}>{answer}</Button>
+                {isTimeOut ?
+                    <Container maxWidth="md" align="center"><h1>You weren't fast enough!</h1></Container> : loading ?
+                        <h1><Loading/></h1> : <>
+                            <h1 align="center">{question.question}</h1>
+                            <Grid
+                                container
+                                textAlign="center"
+                                spacing={3}
+                                columnSpacing={{xs: 1, sm: 2, md: 3}}
+                            >
+                                {question.answers.map((answer, index) => (
+                                    <Grid key={index} item xs={6}>
+                                        <Button variant="contained"
+                                                className={correctAnswerIndex === index ? "correct" : null}
+                                                sx={{width: "100%"}} size="large" key={index}
+                                                onClick={() => handleAnswerSubmit(index)}>{answer}</Button>
+                                    </Grid>
+                                ))}
                             </Grid>
-                        ))}
-                    </Grid>
-                </>}
+                        </>}
             </Container>
-            <Counter time={TIME_FOR_QUESTION} key={loading} onTick={handleTick} onComplete={handleTimeOut} isFinished={isFinished} />
-            <PointDisplay points={points} />
+            <Counter time={TIME_FOR_QUESTION} key={loading} onTick={handleTick} onComplete={handleTimeOut}
+                     isFinished={isFinished}/>
+            <PointDisplay points={points}/>
         </div>
     );
 }
