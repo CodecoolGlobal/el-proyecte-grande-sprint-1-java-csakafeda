893d30e359e4233b9272403fcf9e354ff14d5263
BrownieMaar <barnamarci07@gmail.com>
2023-02-16 07:20:38 +0100

    call service on endpoint

--- backend/src/main/java/com/example/quizio/controller/GameController.java
+++ backend/src/main/java/com/example/quizio/controller/GameController.java
@@ -1,8 +1,14 @@
 package com.example.quizio.controller;
 
+import com.example.quizio.database.enums.Category;
+import com.example.quizio.database.enums.Difficulty;
 import com.example.quizio.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class GameController {
@@ -11,5 +17,14 @@ public class GameController {
     @Autowired
     public GameController(GameService gameService) {
         this.gameService = gameService;
+    }
+
+    @PostMapping("newgame")
+    public Long createNewMultiGame(
+            @RequestParam Optional<Difficulty> difficulty,
+            @RequestParam Optional<Category> category,
+            @RequestParam Optional<Long> createdBy
+    ) {
+        return gameService.generateNewMultiGameAndReturnGameId(difficulty, category, createdBy);
     }
 }
