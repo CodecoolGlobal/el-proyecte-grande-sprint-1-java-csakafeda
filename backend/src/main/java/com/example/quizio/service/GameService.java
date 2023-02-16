454a4d599dfa575c785ff6086cb991f32330bba8
BrownieMaar <barnamarci07@gmail.com>
2023-02-16 07:09:52 +0100

    add PlayerRepo field to GameService

--- backend/src/main/java/com/example/quizio/service/GameService.java
+++ backend/src/main/java/com/example/quizio/service/GameService.java
@@ -1,15 +1,18 @@
 package com.example.quizio.service;
 
 import com.example.quizio.database.GameRepository;
+import com.example.quizio.database.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
     private final GameRepository gameRepository;
+    private final PlayerRepository playerRepository;
 
     @Autowired
-    public GameService(GameRepository gameRepository) {
+    public GameService(GameRepository gameRepository, PlayerRepository playerRepository) {
         this.gameRepository = gameRepository;
+        this.playerRepository = playerRepository;
     }
 }
