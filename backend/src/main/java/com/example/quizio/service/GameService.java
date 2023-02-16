7ecdb00b5ac1b89544cbca069359b0370641c19d
BrownieMaar <barnamarci07@gmail.com>
2023-02-16 07:19:27 +0100

    add handler method to GameService for new game creation

--- backend/src/main/java/com/example/quizio/service/GameService.java
+++ backend/src/main/java/com/example/quizio/service/GameService.java
@@ -2,8 +2,12 @@ package com.example.quizio.service;
 
 import com.example.quizio.database.GameRepository;
 import com.example.quizio.database.PlayerRepository;
+import com.example.quizio.database.enums.Category;
+import com.example.quizio.database.enums.Difficulty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -14,5 +18,13 @@ public class GameService {
     public GameService(GameRepository gameRepository, PlayerRepository playerRepository) {
         this.gameRepository = gameRepository;
         this.playerRepository = playerRepository;
+    }
+
+    public Long generateNewMultiGameAndReturnGameId(
+            Optional<Difficulty> difficulty,
+            Optional<Category> category,
+            Optional<Long> createdBy
+    ) {
+        return null;
     }
 }
