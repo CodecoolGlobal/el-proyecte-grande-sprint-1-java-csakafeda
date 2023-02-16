f380455034b9ace3efa8fc0ddcf8908f7fd94d12
BrownieMaar <barnamarci07@gmail.com>
2023-02-16 07:34:58 +0100

    add builder annotation to Game Entity

--- backend/src/main/java/com/example/quizio/database/repository/Game.java
+++ backend/src/main/java/com/example/quizio/database/repository/Game.java
@@ -1,9 +1,6 @@
 package com.example.quizio.database.repository;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
@@ -14,6 +11,7 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
