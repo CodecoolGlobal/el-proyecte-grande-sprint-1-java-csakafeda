afcbe7179c860174b1f6f352f38cdde76c09a651
BrownieMaar <barnamarci07@gmail.com>
2023-02-16 07:57:44 +0100

    add logic to creating new multigame

--- backend/src/main/java/com/example/quizio/service/GameService.java
+++ backend/src/main/java/com/example/quizio/service/GameService.java
@@ -4,27 +4,40 @@ import com.example.quizio.database.GameRepository;
 import com.example.quizio.database.PlayerRepository;
 import com.example.quizio.database.enums.Category;
 import com.example.quizio.database.enums.Difficulty;
+import com.example.quizio.database.repository.Game;
+import com.example.quizio.database.repository.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class GameService {
+    private final int QUESTION_LENGTH = 10;
     private final GameRepository gameRepository;
     private final PlayerRepository playerRepository;
+    private final QuestionService questionService;
 
     @Autowired
-    public GameService(GameRepository gameRepository, PlayerRepository playerRepository) {
+    public GameService(GameRepository gameRepository, PlayerRepository playerRepository, QuestionService questionService) {
         this.gameRepository = gameRepository;
         this.playerRepository = playerRepository;
+        this.questionService = questionService;
     }
+
 
     public Long generateNewMultiGameAndReturnGameId(
             Long createdBy,
             Optional<Difficulty> difficulty,
             Optional<Category> category
     ) {
-        return null;
+        Question[] questions = questionService.getMultipleQuestions(QUESTION_LENGTH, difficulty, category);
+        Game.GameBuilder gameBuilder = Game.builder().questions(List.of(questions)).creator(playerRepository.getById(createdBy));
+        difficulty.ifPresent(gameBuilder::difficulty);
+        category.ifPresent(gameBuilder::category);
+        Game game = gameBuilder.build();
+        gameRepository.save(game);
+        return game.getId();
     }
 }
