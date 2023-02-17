6d08c3b64b4fa1d2e5e2ebf9b44f25e0a912cc39
BrownieMaar <barnamarci07@gmail.com>
2023-02-17 08:02:04 +0100

    implement localstorage methods in userTools

--- frontend/src/Tools/userTools.js
+++ frontend/src/Tools/userTools.js
@@ -1,3 +1,4 @@
-export const isUserSignedIn = () => false;
+export const isUserSignedIn = () => localStorage.getItem("playerId") === null;
 export const getUserName = () => "Csincsilla";
-export const getPlayerId = () => 1234;
+export const getPlayerId = () => localStorage.getItem("playerId");
+export const setPlayerId = (playerId) => localStorage.setItem("playerId", playerId)
