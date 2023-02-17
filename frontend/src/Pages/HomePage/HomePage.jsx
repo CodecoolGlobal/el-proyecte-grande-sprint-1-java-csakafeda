d12599fb7c4af9fa82787f29de02e5191419e902
BrownieMaar <barnamarci07@gmail.com>
2023-02-17 05:36:18 +0100

    run style formatter in vscode in homepage

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -9,9 +9,9 @@ import {
     OutlinedInput,
     useTheme
 } from "@mui/material";
-import {Container} from "@mui/system";
-import {useNavigate} from "react-router-dom";
-import {useState} from "react";
+import { Container } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -56,7 +56,7 @@ export default function HomePage() {
         categories.map(category => category.charAt(0).toUpperCase()
             + category.slice(1).replace(/_/g, " "));
     const handleCategoryChange = (event) => {
-        const {target: {value}} = event;
+        const { target: { value } } = event;
         setChosenCategory(typeof value === 'string' ? value.split(',') : value);
     };
 
@@ -85,20 +85,20 @@ export default function HomePage() {
     }
 
     return <>
-        <Container align="center" sx={{padding: "2rem"}}>
+        <Container align="center" sx={{ padding: "2rem" }}>
             <Button variant="contained"
-                    size="large"
-                    sx={{margin: "2rem", padding: "2rem"}}
-                    onClick={() => {
-                        navigate("search-multi/")
-                    }
-                    }
+                size="large"
+                sx={{ margin: "2rem", padding: "2rem" }}
+                onClick={() => {
+                    navigate("search-multi/")
+                }
+                }
             >
                 Search existing games
             </Button>
         </Container>
-        <Container align="center" sx={{padding: "5rem"}}>
-            <FormControl sx={{m: 1, minWidth: 300}}>
+        <Container align="center" sx={{ padding: "5rem" }}>
+            <FormControl sx={{ m: 1, minWidth: 300 }}>
                 <InputLabel id="difficulty-choosing">
                     Difficulty
                 </InputLabel>
@@ -114,7 +114,7 @@ export default function HomePage() {
                 </Select>
             </FormControl>
 
-            <FormControl sx={{m: 1, width: 300}}>
+            <FormControl sx={{ m: 1, width: 300 }}>
                 <InputLabel id="categories">Categories</InputLabel>
                 <Select
                     labelId="categories"
@@ -122,11 +122,11 @@ export default function HomePage() {
                     multiple
                     value={chosenCategory}
                     onChange={handleCategoryChange}
-                    input={<OutlinedInput id="select-multiple-category" label="categories"/>}
+                    input={<OutlinedInput id="select-multiple-category" label="categories" />}
                     renderValue={(selected) => (
-                        <Box sx={{display: 'flex', flexWrap: 'wrap', gap: 0.5}}>
+                        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
                             {selected.map((value) => (
-                                <Chip key={value} label={value}/>
+                                <Chip key={value} label={value} />
                             ))}
                         </Box>
                     )}
@@ -145,8 +145,8 @@ export default function HomePage() {
             </FormControl>
         </Container>
 
-        <Container align="center" sx={{padding: "4rem"}}>
-            <Button variant="contained" size="large" sx={{margin: "4rem", padding: "2rem"}} onClick={() => {
+        <Container align="center" sx={{ padding: "4rem" }}>
+            <Button variant="contained" size="large" sx={{ margin: "4rem", padding: "2rem" }} onClick={() => {
                 navigate({
                     pathname: "/question-single",
                     search: getCategoryAndDifficultySearchParam()
@@ -155,14 +155,14 @@ export default function HomePage() {
             }>
                 Single player game
             </Button>
-            <Button variant="contained" size="large" sx={{margin: "4rem", padding: "2rem"}} gameid={gameId}
-                    onClick={(e) => {
-                        getCategoryAndDifficultySearchParam(e).then((r) => {
-                            setGameId(r);
-                            navigate("question-multi/");
-                        });
-                    }
-                    }>
+            <Button variant="contained" size="large" sx={{ margin: "4rem", padding: "2rem" }} gameid={gameId}
+                onClick={(e) => {
+                    getCategoryAndDifficultySearchParam(e).then((r) => {
+                        setGameId(r);
+                        navigate("question-multi/");
+                    });
+                }
+                }>
                 Multiplayer game
             </Button>
         </Container>
