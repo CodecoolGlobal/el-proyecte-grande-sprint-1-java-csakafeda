4a6db527ffd60da2f86497c3438ca8127c87bf2e
BrownieMaar <barnamarci07@gmail.com>
2023-02-17 07:31:49 +0100

    add page structure with mui to authpages

--- frontend/src/Pages/Auth/LogIn.jsx
+++ frontend/src/Pages/Auth/LogIn.jsx
@@ -1,4 +1,29 @@
-export default function LogIn() {
+import { Box, Button, Container, Stack, TextField, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-    return <>LogIn</>
+export default function LogIn() {
+    const navigate = useNavigate();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const entries = [...formData.entries()]
+        console.log(entries);
+    }
+
+    return <Container maxWidth={"sm"} sx={{marginBlock: 2}}>
+        <Box onSubmit={handleSubmit} component={"form"}>
+        <Stack gap={2}>
+            <Typography variant={"h4"} align="center">Log In</Typography>
+            <TextField variant={"outlined"} name={"Username"} label="Username"></TextField>
+            <TextField variant={"outlined"} name={"Email"} label="Email"></TextField>
+            <TextField variant={"outlined"} name={"Password"} label="Password" type={"password"}></TextField>
+            <Stack direction={"row"} gap={2}>
+                <Button variant={"contained"} color={"primary"} sx={{flexGrow: 1}}>Submit</Button>
+                <Button variant={"contained"} color={"secondary"} onClick={() => navigate("/signup")}>Sign up instead</Button>
+            </Stack>
+        </Stack>
+
+        </Box>
+    </Container>
 }
--- frontend/src/Pages/Auth/SignUp.jsx
+++ frontend/src/Pages/Auth/SignUp.jsx
@@ -1,4 +1,29 @@
-export default function SignUp() {
+import { Box, Button, Container, Stack, TextField, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-    return <>SignUp</>
+export default function SignUp() {
+    const navigate = useNavigate();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const entries = [...formData.entries()]
+        console.log(entries);
+    }
+
+    return <Container maxWidth={"sm"} sx={{marginBlock: 2}}>
+        <Box onSubmit={handleSubmit} component={"form"}>
+        <Stack gap={2}>
+            <Typography variant={"h4"} align="center">Sign Up</Typography>
+            <TextField variant={"outlined"} name={"Username"} label="Username"></TextField>
+            <TextField variant={"outlined"} name={"Email"} label="Email"></TextField>
+            <TextField variant={"outlined"} name={"Password"} label="Password" type={"password"}></TextField>
+            <Stack direction={"row"} gap={2}>
+                <Button variant={"contained"} color={"primary"} onClick={() => navigate("/login")}>Log in instead</Button>
+                <Button variant={"contained"} color={"secondary"} sx={{flexGrow: 1}}>Submit</Button>
+            </Stack>
+        </Stack>
+
+        </Box>
+    </Container>
 }
