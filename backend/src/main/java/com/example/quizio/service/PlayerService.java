1c8a48d3d0ed63f219c53d5ccaaab44b88d57cfe
BrownieMaar <barnamarci07@gmail.com>
2023-02-17 07:56:04 +0100

    add handling for when username is not in db

--- backend/src/main/java/com/example/quizio/service/PlayerService.java
+++ backend/src/main/java/com/example/quizio/service/PlayerService.java
@@ -1,6 +1,7 @@
 package com.example.quizio.service;
 
 import com.example.quizio.controller.exception.PasswordDoesNotMatchException;
+import com.example.quizio.controller.exception.UsernameNotFoundException;
 import com.example.quizio.database.PlayerRepository;
 import com.example.quizio.database.repository.Player;
 import org.springframework.stereotype.Service;
@@ -18,10 +19,17 @@ public class PlayerService {
     }
 
     public Long getIdFromPlayer(Player player) {
+
+        if (!playerRepository.existsByName(player.getName())) {
+            throw new UsernameNotFoundException("Username " + player.getName() + "was not found in database.");
+        }
+
         Player fullPlayerEntity = playerRepository.getPlayerByName(player.getName());
+
         if (!player.getPassword().equals(fullPlayerEntity.getPassword())) {
             throw new PasswordDoesNotMatchException("Provided passwords do not match.");
         }
+
         return fullPlayerEntity.getId();
     }
 }
