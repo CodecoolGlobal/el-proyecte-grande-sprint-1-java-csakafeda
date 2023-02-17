5a88061331bdadf76d4ebc067693f508dd65fec3
BrownieMaar <barnamarci07@gmail.com>
2023-02-17 08:11:16 +0100

    fix localstorage sjon issue

--- frontend/src/Tools/userTools.js
+++ frontend/src/Tools/userTools.js
@@ -1,4 +1,5 @@
-export const isUserSignedIn = () => localStorage.getItem("playerId") === null;
+export const isUserSignedIn = () => localStorage.getItem("playerId") !== null;
 export const getUserName = () => "Csincsilla";
 export const getPlayerId = () => localStorage.getItem("playerId");
-export const setPlayerId = (playerId) => localStorage.setItem("playerId", playerId)
+export const setPlayerId = (playerId) => localStorage.setItem("playerId", JSON.stringify(playerId))
+export const signUserOut = () => localStorage.removeItem("playerId");
