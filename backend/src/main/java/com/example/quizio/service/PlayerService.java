b1c9f801224f8e98002c9a28f00e03e39832c6dc
bridgend84 <sacrun@gmail.com>
2023-02-27 14:40:12 +0100

    add TODO-s, make override methods in security config class

--- backend/src/main/java/com/example/quizio/service/PlayerService.java
+++ backend/src/main/java/com/example/quizio/service/PlayerService.java
@@ -4,10 +4,12 @@ import com.example.quizio.controller.exception.PasswordDoesNotMatchException;
 import com.example.quizio.controller.exception.UsernameNotFoundException;
 import com.example.quizio.database.PlayerRepository;
 import com.example.quizio.database.repository.Player;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PlayerService {
+public class PlayerService implements UserDetailsService {
     private final PlayerRepository playerRepository;
 
     public PlayerService(PlayerRepository playerRepository) {
@@ -31,5 +33,11 @@ public class PlayerService {
         }
 
         return fullPlayerEntity.getId();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+        //TODO
     }
 }
