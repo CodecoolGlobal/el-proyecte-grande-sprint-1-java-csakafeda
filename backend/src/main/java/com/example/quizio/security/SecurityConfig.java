432be823ebd90d7a4355851a84d4708513830ab7
bridgend84 <sacrun@gmail.com>
2023-02-27 14:16:07 +0100

    add security dependency and create config class

---
+++ backend/src/main/java/com/example/quizio/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.example.quizio.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
