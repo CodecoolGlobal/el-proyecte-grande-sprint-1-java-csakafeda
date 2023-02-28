21422dc3dc68a43c2b3e9320c2432ebc919e7f8a
Feodóra Bakó <bako.feodora97@gmail.com>
2023-02-28 13:23:42 +0100

    override attemptAuthentication method in authenticationFilter class

---
+++ backend/src/main/java/com/example/quizio/security/filter/AuthenticationFilter.java
@@ -0,0 +1,37 @@
+package com.example.quizio.security.filter;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response)
+            throws AuthenticationException {
+        String playerName = request.getParameter("name");
+        String password = request.getParameter("password");
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(playerName, password);
+        return authenticationManager.authenticate(token);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
