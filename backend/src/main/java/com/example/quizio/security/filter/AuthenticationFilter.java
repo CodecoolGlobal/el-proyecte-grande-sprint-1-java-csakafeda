74263c111f9cf8be35ad21e79cfb8e392094ab61
Feodóra Bakó <bako.feodora97@gmail.com>
2023-02-28 14:08:32 +0100

    complete successfulAuthentication in AuthenticationFilter class (without error handling)

--- backend/src/main/java/com/example/quizio/security/filter/AuthenticationFilter.java
+++ backend/src/main/java/com/example/quizio/security/filter/AuthenticationFilter.java
@@ -1,16 +1,24 @@
 package com.example.quizio.security.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -31,7 +39,30 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult)
+            throws IOException, ServletException {
+        User player = (User) authResult.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256(player.getUsername());
+        List<String> authorityList = player.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        String token = JWT.create()
+                .withSubject(player.getUsername())
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", authorityList)
+                .sign(algorithm);
+
+        Cookie cookie = new Cookie("token", token);
+        cookie.setSecure(true);
+
+        response.addCookie(cookie);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(200);
+
+        //TODO error handling
     }
 }
