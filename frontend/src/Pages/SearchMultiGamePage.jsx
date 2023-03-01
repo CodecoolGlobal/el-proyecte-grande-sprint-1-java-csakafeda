a4c64e408b58892740f8d04396aa9a50edb4bdd5
BrownieMaar <barnamarci07@gmail.com>
2023-03-01 14:25:15 +0100

    refactor game search  to form for accessability

--- frontend/src/Pages/SearchMultiGamePage.jsx
+++ frontend/src/Pages/SearchMultiGamePage.jsx
@@ -1,25 +1,25 @@
 import {
+    Box,
     Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
     TextField
 } from "@mui/material";
-import {Container} from "@mui/system";
-import {useState} from "react";
+import { Container } from "@mui/system";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function SearchMultiGamePage() {
-    const [search, setSearch] = useState("");
     const [games, setGames] = useState(null);
     const navigate = useNavigate();
 
-    const fetchGames = async () => {
-        const res = await fetch(`/api/loadgame?name=${search}`)
+    const fetchGames = async (name) => {
+        const res = await fetch(`/api/loadgame?name=${name}`)
         const data = await res.json();
         return data;
     }
 
-    const handleSearch = async () => {
+    const handleSearch = async (name) => {
         try {
-            const res = await fetchGames(search);
+            const res = await fetchGames(name);
             if (res.length > 0) {
                 setGames(res);
             } else {
@@ -30,29 +30,47 @@ export default function SearchMultiGamePage() {
         }
     }
 
-    const handleSearchTyping = (e) => {
-        setSearch(e.target.value);
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const entries = [...formData.entries()]
+        const form = entries.reduce((acc, curr) => {
+            acc[curr[0]] = curr[1];
+            return acc;
+        }, {})
+        console.log(form);
+        handleSearch(form.name);
     }
 
     return <>
-        <Container align="center" sx={{padding: "2rem"}}>
-            <TextField sx={{width: '30rem'}}
-                       id="outlined-basic"
-                       label="Game search"
-                       variant="outlined"
-                       placeholder="Search game by player name"
-                       onChange={handleSearchTyping}
-            />
-        </Container>
-        <Container align="center" sx={{padding: "0.5rem"}}>
-            <Button variant="contained"
+        <Container align="center" sx={{ padding: "2rem" }}>
+            <Box component={"form"} onSubmit={e => handleFormSubmit(e)}>
+                <TextField
+                    sx={{ width: '20rem', marginRight: ".5rem" }}
+                    name="name"
+                    label="Name"
+                    variant="outlined"
+                    placeholder="Search game by player name"
+                />
+                {/* <TextField
+                    sx={{ width: '20rem' }}
+                    name="gameId"
+                    label="Game Id"
+                    variant="outlined"
+                    placeholder="Search game by game id"
+                /> */}
+                <Button
+                    variant="contained"
                     size="small"
-                    sx={{margin: "0.5rem", padding: "1rem"}}
-                    onClick={handleSearch}>
-                Search players games
-            </Button>
+                    sx={{ margin: "0 .5rem", padding: "1rem" }}
+                    type={"submit"}>
+                    Search games
+                </Button>
+            </Box>
         </Container>
-        <Container align="center" sx={{padding: "2rem"}}>
+        <Container align="center" sx={{ padding: "0.5rem" }}>
+        </Container>
+        <Container align="center" sx={{ padding: "2rem" }}>
             <TableContainer align="center">
                 <Table align="center">
                     <TableHead>
@@ -66,20 +84,20 @@ export default function SearchMultiGamePage() {
                     <TableBody>
                         {games !== null ?
                             games.map(game => (
-                                    <TableRow key={game.id}>
-                                        <TableCell>{game.id}</TableCell>
-                                        <TableCell>{game.difficulty}</TableCell>
-                                        <TableCell>{game.highscore}</TableCell>
-                                        <Button variant="contained"
-                                                align="right"
-                                                size="small"
-                                                sx={{margin: "0.5rem", padding: "0.5rem"}}
-                                                onClick={() => navigate("/question-multi/" + game.id)}
-                                                >
-                                            Start this game!
-                                        </Button>
-                                    </TableRow>
-                                )
+                                <TableRow key={game.id}>
+                                    <TableCell>{game.id}</TableCell>
+                                    <TableCell>{game.difficulty}</TableCell>
+                                    <TableCell>{game.highscore}</TableCell>
+                                    <Button variant="contained"
+                                        align="right"
+                                        size="small"
+                                        sx={{ margin: "0.5rem", padding: "0.5rem" }}
+                                        onClick={() => navigate("/question-multi/" + game.id)}
+                                    >
+                                        Start this game!
+                                    </Button>
+                                </TableRow>
+                            )
                             )
                             :
                             <></>
