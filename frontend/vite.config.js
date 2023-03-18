d92bf78424a747857f8d9357fbd8c656fe141f7a
bridgend84 <sacrun@gmail.com>
2023-03-18 22:57:59 +0100

    add frontend dockerfiles, move compose file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
     proxy: {
       "/api": {
         target: "http://localhost:8080",
